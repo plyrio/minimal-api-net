070930dcb88425528c2b5081275e38d3c871d58d
Pedro Lyrio <138774252+plyrio@users.noreply.github.com>
2025-11-01 08:27:29 +0000

    feat(migrations): adiciona dados iniciais para a tabela Administrators

--- Infrastructure/Db/AppDbContext.cs
+++ Infrastructure/Db/AppDbContext.cs
@@ -12,6 +12,20 @@ public class AppDbContext : DbContext
     }
     public DbSet<Administrator> Administrators { get; set; } = default!;
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrator>().HasData(
+            new Administrator
+            {
+                Id = 1,
+                Email = "admin@teste.com",
+                Password = "123456",
+                Profile = "ADMIN"
+            }
+        );
+        
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
