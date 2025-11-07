fcd9425485300bcac7a4baaa0fb6680736dd02a1
Pedro Lyrio <138774252+plyrio@users.noreply.github.com>
2025-11-07 10:26:31 +0000

    feat: adiciona estrutura Home e implementa endpoint de login para administradores

---
+++ Domain/ModelViews/Home.cs
@@ -0,0 +1,6 @@
+namespace MinimalAPI.Domain.ModelViews;
+
+public struct Home
+{
+    public string Doc { get => "/swagger"; }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using MinimalAPI.Domain.DTOs;
 using MinimalAPI.Infrastructure.Db;
 using MinimalAPI.Domain.Services;
 using MinimalAPI.Domain.Interfaces;
+using MinimalAPI.Domain.ModelViews;
 using Microsoft.AspNetCore.Mvc;
 
 DotNetEnv.Env.Load();
@@ -12,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministratorService, AdministratorService>();
 
+#region Builder
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,22 +25,32 @@ builder.Services.AddDbContext<AppDbContext>(
 );
 
 var app = builder.Build();
+#endregion
 
+#region  Home
+app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-
-app.MapGet("/", () => "Hello World!");
-
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministratorService administratorService) =>
+#region Administrators
+app.MapPost("/administrators/login", ([FromBody] LoginDTO loginDTO, IAdministratorService administratorService) =>
 {
     if (administratorService.Login(loginDTO) != null)
         return Results.Ok("Login successful");
     else
         return Results.Unauthorized();
 });
+#endregion
 
+
+#region Vehicle
+
+#endregion
+
+
+#region App
 app.UseSwagger();
 app.UseSwaggerUI();
 
 
 app.Run();
-
+#endregion
